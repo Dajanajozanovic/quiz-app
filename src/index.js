7523f3f79f1faa8a52a60013294e51e70f466dbc
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-04-27 18:32:54 +0200

    Added html pages to index.html

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const buttonHome = document.querySelector('.button-home')
+const buttonBookmarks = document.querySelector('.button-bookmarks')
+const buttonCreate = document.querySelector('.button-create')
+const buttonSettings = document.querySelector('.button-settings')
+
+const homePage = document.querySelector('.page-home')
+const createPage = document.querySelector('.page-create')
+const bookmarksPage = document.querySelector('.page-bookmarks')
+const profilePage = document.querySelector('.page-settings')
+
+buttonHome.addEventListener('click', () => {
+  homePage.classList.remove('hidden')
+  createPage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+  buttonHome.classList.add('active')
+  buttonCreate.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonBookmarks.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  bookmarksPage.classList.remove('hidden')
+  profilePage.classList.add('hidden')
+  buttonHome.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonBookmarks.classList.add('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonCreate.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  createPage.classList.remove('hidden')
+  bookmarksPage.classList.add('hidden')
+  profilePage.classList.add('hidden')
+  buttonHome.classList.remove('active')
+  buttonCreate.classList.add('active')
+  buttonBookmarks.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonProfile.addEventListener('click', () => {
+  homePage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  profilePage.classList.remove('hidden')
+  buttonHome.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonProfile.classList.add('active')
+})
